11dfa06dae7bbc04fba7c4a306c2bc11b8cb344b
acerise <42525915+acerise85@users.noreply.github.com>
2019-02-11 20:46:23 -0700

    Calculations working, nearly completed app

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,38 @@
+var express =require("express");
+var path = require("path");
+var router = express.Router();
+var friends = require("../data/friends");
+
+
+router.get("/api/friends", function(req, res) {
+    // res.send
+    res.json(friends);
+  });
+
+router.post("/api/friends",function(req, res){
+
+    var newFriendScores = req.body.scores
+    for (let i = 0; i < friends.length; i++) {
+        var someNumber = 100
+        var chosenFriendIndex
+        var diff = 0
+        for (let j = 0; j < friends[i].scores.length; j++) {
+           diff += Math.abs(friends[i].scores[j], newFriendScores[j])
+        }  
+        console.log('diff',diff);
+        if (diff < someNumber){
+            someNumber = diff
+            chosenFriendIndex = i
+        }
+        console.log('someNumbers', someNumber);
+        console.log('chosenFriendIndex', chosenFriendIndex);
+        
+        
+        
+    }
+    res.send(friends[chosenFriendIndex])
+})
+
+  module.exports = router;
+
+
---
+++ server.js
@@ -0,0 +1,23 @@
+
+
+let express = require("express");
+var htmlRoutes = require("./app/routing/htmlRoutes")
+var apiRoutes = require("./app/routing/apiRoutes")
+
+
+var app = express();
+var PORT = 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(htmlRoutes);
+app.use(apiRoutes);
+
+
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
