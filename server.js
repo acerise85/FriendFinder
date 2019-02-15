3cfa0b23f9474c29385176c448df8740cd7bdc32
acerise <42525915+acerise85@users.noreply.github.com>
2019-02-14 21:56:40 -0700

    port

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var apiRoutes = require("./app/routing/apiRoutes")
 
 
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
